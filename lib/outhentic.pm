50e4147c43ca5f4c6df8583d6b8e2d39ef0e84fa
Alexey Melezhik <melezhik@gmail.com>
2015-10-19 12:39:28 +0300

    cpan distro

---
+++ lib/outhentic.pm
@@ -0,0 +1,27 @@
+package outhentic;
+
+our $VERSION = '0.00001';
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+Print something into stdout and test
+
+
+=head1 Documentation
+
+Please follow github pages  - https://github.com/melezhik/outhentic
+
+=head1 AUTHOR
+
+Aleksei Melezhik
+
+=head1 COPYRIGHT
+
+Copyright 2015 Alexey Melezhik.
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+

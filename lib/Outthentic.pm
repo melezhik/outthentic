fcbe8ebd2c1acd2dd3f64190b2c4e6cb2b937753
Alexey Melezhik <melezhik@gmail.com>
2016-04-18 15:12:01 +0300

    up

--- lib/Outthentic.pm
+++ lib/Outthentic.pm
@@ -48,16 +48,24 @@ sub config {
     my @runtime_params = split /:::/, get_prop('runtime_params');
 
     PARAM: for my $rp (@runtime_params){
-      my $root = $config;
+
       my $value;
-      for my $path (split /\./, $rp ){
-        if ($path=~s/=(.*)//){
-          $value = $1;
-        }  
+
+      if ($rp=~s/=(.*)//){
+        $value = $1;
+      }else{
+        next PARAM;
+      }  
+
+      my @pathes = split /\./, $rp;
+      my $last_path = pop @pathes;
+
+      my $root = $config;
+      for my $path (@pathes){
         next PARAM unless defined $root->{$path};
-        $root->{$path} = $value if defined $value;
         $root = $root->{$path};
       }
+      $root->{$last_path} = $value;
     }
 
     return $config;

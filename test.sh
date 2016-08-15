6e1f17512cc0e494b46d2de854e5b48010b5c025
melezhik <melezhik@gmail.com>
2016-08-15 15:16:38 +0300

    fix for travis

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 start_with=$1;
-start_with=${start_with:-.};
-OUTTHENTIC_ROOT=`pwd` && find $start_with -name story.rb -or -name story.bash -or -name story.pl -execdir pwd \; | grep -v modules/ | perl -n -e 'chomp; s{.*examples/}[]; push @foo, "strun --root examples --story $_"; END { print join " && " , @foo }' | bash
+start_with=${start_with:-examples};
+find $start_with -name story.rb -or -name story.bash -or -name story.pl -execdir pwd \; | grep -v modules/ | perl -n -e 'chomp; s{.*examples/}[]; push @foo, "strun --root examples --story $_"; END { print join " && " , @foo }' | bash

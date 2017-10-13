ffd609d24ca490f3851c44ac39382b04847b8ae2
Alexey Melezhik <melezhik@gmail.com>
2017-10-13 15:41:14 +0300

    when running by sh ( not bash ) `type $command` generates STDERR "unknown command $command" if from unexisted $command despite we redirect STDERR to /dev/null via `>/dev/null 2>&1`

--- lib/Outthentic/Story.pm
+++ lib/Outthentic/Story.pm
@@ -741,7 +741,7 @@ if [ -s /etc/os-release ]; then
   . /etc/os-release
   OS=$NAME
   VER=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
+elif lsb_release -h >/dev/null 2>&1; then
   # linuxbase.org
   OS=$(lsb_release -si)
   VER=$(lsb_release -sr)

6d9d96063e5459c60501c846823af00b84fc06b2
Alexey Melezhik <melezhik@gmail.com>
2018-09-24 21:35:48 +0000

    recoursive

--- dsl-test.pl
+++ dsl-test.pl
@@ -3,21 +3,5 @@
 use File::Find;
 use Cwd;
 
-my $root = getcwd();
-
-find( { wanted => \&wanted, no_chdir => 1 } , $ARGV[0]||'dsl-test/');
-
-sub wanted  {
-
-  return unless /story\.(pl|rb|bash)$/ or /meta\.txt$/;
-
-  return if /modules\//;
-
-  (my $dir = $File::Find::dir)=~s{dsl-test/}{};
-
-  my $cmd = "strun --purge-cache --root ./dsl-test --story $dir --format default --debug 0";
-  (system($cmd) == 0)  or die "$cmd failed";
-
-}
-
-
+my $cmd = "cd dsl-test && strun --recourse --purge-cache  --format production";
+(system($cmd) == 0)  or die "$cmd failed";

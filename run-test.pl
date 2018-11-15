c4ce319bc6c2736c97b872af665282237e6a3bd1
Melezhik, Aleksei <gimy@chevron.com>
2018-11-15 17:06:28 -0600

    fix windows tests

--- run-test.pl
+++ run-test.pl
@@ -12,19 +12,29 @@ if ($^O  =~ 'MSWin'){
   (system($cmd) == 0)  or die "$cmd failed";
 }
 
+my %seen;
+
 sub wanted  {
 
-  return unless /story\.(pl|rb|bash|py|pm|ps1)$/ or /meta\.txt$/;
 
-  return if /modules\//;
+  return unless $File::Find::name=~/(story|hook)\.(pl|rb|bash|py|pm|ps1)$/ or /meta\.txt$/;
 
-  return unless ( -e $File::Find::dir."\\windows.test" or $File::Find::dir =~/windows/ );
+  return unless -e $File::Find::dir."/windows.test";
 
   (my $dir = $File::Find::dir)=~s{examples/}{};
+
+  return if $dir=~/modules/;
+
+  return if $seen{$dir};
   
   my $cmd = "cd examples && strun --purge-cache --story $dir --format production";
 
   (system($cmd) == 0)  or die "$cmd failed";
+
+  #print "$cmd ...\n";
+  #print "\n";
+
+  $seen{$dir}++;
 
 }
 
